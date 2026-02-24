ec77a1b0198058782089737c9193a7da3a322145
ManuelGarcia <manueldev250799@gmail.com>
2026-02-23 23:18:30 -0500

    feat: reorganize imports in CommonModule and add SQSService to providers

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
-import { CommonController } from './common.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from '@/transactions/entities/transaction.entity';
-import { StatisticsService } from './services/statistics.service';
+import { AccountsModule } from '@/accounts/accounts.module';
 import UserDevice from '@/accounts/entities/user-device.entity';
+import { CommonController } from './common.controller';
+import SQSService from './services/sqs.service';
+import { StatisticsService } from './services/statistics.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction, UserDevice])],
+  imports: [
+    TypeOrmModule.forFeature([Transaction, UserDevice]),
+    AccountsModule,
+  ],
   controllers: [CommonController],
-  providers: [StatisticsService],
+  providers: [StatisticsService, SQSService],
 })
 export class CommonModule {}
